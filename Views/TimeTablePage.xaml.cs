98866040171537ab16964bfc4c4cde74071b1dbb
MetaException <0x0_email_address_not_found@mail.ru>
2023-10-18 00:06:55 +0300

    cleanup and complete timetable parsing

--- Views/TimeTablePage.xaml.cs
+++ Views/TimeTablePage.xaml.cs
@@ -8,6 +8,12 @@ public partial class TimeTablePage : ContentPage
 {
     private readonly ObservableCollection<TimeTableSubject> subjectList;
 
+    private DateTime currDay;
+    private DateTime firstDayOfWeek;
+    private int currWeekNumber;
+
+    //Добавить кнопку для возврата в текущий день
+
     private string currWeek;
 
     public TimeTablePage()
@@ -15,36 +21,64 @@ public partial class TimeTablePage : ContentPage
         InitializeComponent();
 
         subjectList = new ObservableCollection<TimeTableSubject>();
-        currentDayLabel.Text = DateTime.Now.ToShortDateString();
+        currDay = DateTime.Now;
 
         ParseAsync();
     }
 
-    private async Task ParseCurrentWeekAsync()
+    private async void ParseAsync()
     {
         var weekList = await TimeTableParser.ParseWeekList();
 
-        DateTime cw = new DateTime();
-        int weekNumber = 0;
+        DateTime currentDay = DateTime.Now.Date;
 
         for (int i = 0; i < weekList.Length; i++)
         {
-            if (weekList[i].Date > DateTime.Now.Date)
+            if (weekList[i].Date > currentDay)
             {
-                cw = weekList[i - 1];
-                weekNumber = i;
+                firstDayOfWeek = weekList[i - 1];
+                currWeekNumber = i;
                 break;
             }
         }
 
-        currWeek = $"{weekNumber} - {cw.Year}/{cw.Month}/{cw.Day}";
+        currWeek = $"{currWeekNumber} - {firstDayOfWeek.Year}/{firstDayOfWeek.Month}/{firstDayOfWeek.Day}";
+        currDay = currentDay;
+
+        UpdateDaysList();
+        UpdateTimeTable(currDay);
     }
 
-    private async void ParseAsync()
+    private void Rb_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-        await ParseCurrentWeekAsync();
+        if (e.Value)
+        {
+            var date = new DateTime(currDay.Year, currDay.Month, int.Parse(((RadioButton)sender).ContentAsString()));
+            currDay = date;
+            //Изменять неделю тоже??
+            UpdateTimeTable(date);
+        }
+    }
 
-        var timeTableParser = TimeTableParser.ParseInfoByDay(currentDayLabel.Text);
+    private async void UpdateDaysList()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            var rb = new RadioButton() { Content = firstDayOfWeek.AddDays(i).Day, IsChecked = firstDayOfWeek.AddDays(i).Day == currDay.Day };
+            rb.CheckedChanged += Rb_CheckedChanged;
+            DateList.Add(rb);
+        }
+    }
+
+    private async void UpdateTimeTable(DateTime date)
+    {
+        subjectList.Clear();
+        TimeTable.ItemsSource = subjectList;
+        crday.Text = currDay.ToString();
+        crweek.Text = currWeekNumber.ToString();
+
+        var timeTableParser = TimeTableParser.ParseInfoByDay(date.ToShortDateString());
+
         //var timeTableParser = TimeTableParser.ParseInfoByWeek(currWeek);
 
         await foreach (var subjectInfo in timeTableParser)
@@ -52,5 +86,35 @@ public partial class TimeTablePage : ContentPage
             subjectList.Add(subjectInfo);
         }
         TimeTable.ItemsSource = subjectList;
+    }
+
+    private void GoBackWeek_Clicked(object sender, EventArgs e)
+    {
+        if (currWeekNumber <= 1)
+            return;
+
+        currDay = currDay.AddDays(-7);
+        firstDayOfWeek = firstDayOfWeek.AddDays(-7);
+        currWeekNumber--;
+
+        DateList.Clear();
+
+        UpdateDaysList();
+        UpdateTimeTable(currDay);
+    }
+
+    private void GoNextWeek_Clicked(object sender, EventArgs e)
+    {
+        if (currWeekNumber >= 34)
+            return;
+
+        currDay = currDay.AddDays(7);
+        firstDayOfWeek = firstDayOfWeek.AddDays(7);
+        currWeekNumber++;
+
+        DateList.Clear();
+
+        UpdateDaysList();
+        UpdateTimeTable(currDay);
     }
 }
