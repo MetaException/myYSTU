4887f8961476f7f58f9fbded1fae69c5daee685d
MetaException <0x0_email_address_not_found@mail.ru>
2023-11-21 00:23:22 +0300

    Save login and password in secure storage

--- Views/AuthPage.xaml.cs
+++ Views/AuthPage.xaml.cs
@@ -4,34 +4,42 @@ namespace myYSTU.Views;
 
 public partial class AuthPage : ContentPage
 {
-    private readonly INetUtils _netUtil;
+    private readonly INetUtils _netUtil = DependencyService.Get<INetUtils>();
 
     public AuthPage()
     {
         InitializeComponent();
-        _netUtil = DependencyService.Get<INetUtils>();
+
+        TryAuthorizeWithSavedCredentials();
     }
 
-    private async void OnLoginClicked(object sender, EventArgs e)
+    private async void TryAuthorizeWithSavedCredentials()
+    {
+        string login = await SecureStorage.Default.GetAsync("login");
+        string password = await SecureStorage.Default.GetAsync("password");
+
+        if (login != null && password != null)
+            await handleAuthorization(login, password);
+    }
+
+    private async Task handleAuthorization(string Login, string Password)
     {
         LoginBtn.IsEnabled = false; //Выключаем, чтобы пользователь не нажал на кнопку дважды
-
-        string Login = LoginEntry.Text;
-        string Password = PasswordEntry.Text;
-
-        //Для отладки
-        //TODO: убрать
-        Login = "baranovea.21";
-        Password = "OO5n8n#1NP";
 
         var authResult = await _netUtil.Authorize(Login, Password);
 
         if (authResult == 1)
         {
+            await SecureStorage.Default.SetAsync("login", Login);
+            await SecureStorage.Default.SetAsync("password", Password);
+
             App.Current.MainPage = new NavigationPage(new MainPage());
         }
         else if (authResult == 0)
         {
+            SecureStorage.Default.Remove("login");
+            SecureStorage.Default.Remove("password");
+
             errorLabel.TextColor = Colors.Red;
             errorLabel.Text = "Неправильный логин или пароль";
         }
@@ -42,6 +50,15 @@ public partial class AuthPage : ContentPage
             errorLabel.Text = "Произошла ошибка / отсутствует подключение к интернету";
         }
 
+
         LoginBtn.IsEnabled = true;
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        string Login = LoginEntry.Text;
+        string Password = PasswordEntry.Text;
+
+        await handleAuthorization(Login, Password);
     }
 }
